859e86004284975df02f420615a907e00bb0e128
tarequemusa <tareque.web@gmail.com>
2025-11-13 20:49:08 +0600

    Modal Edit Done

--- src/components/Tasks/TaskBoard/TaskBoard.jsx
+++ src/components/Tasks/TaskBoard/TaskBoard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-
+import AddTaskModal from "../AddTaskModal/AddTaskModal";
 import SearchTask from "../SearchTask/SearchTask";
-import TaskAction from "../TaskAction/TaskAction";
+import TaskActions from "../TaskAction/TaskAction";
 import TaskList from "../TaskList/TaskList";
 
 export default function TaskBoard() {
@@ -14,17 +14,43 @@ export default function TaskBoard() {
     priority: "High",
     isFavorite: true,
   };
-  const [tasks, seTasks] = useState([defaultTask]);
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
+
+  function handleAddTask(newTask, isAdd) {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
+    setShowAddModal(false);
+  }
+
+  function handleEditTask(task) {
+    setTaskToUpdate(task);
+    setShowAddModal(true);
+  }
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && (
+        <AddTaskModal onSave={handleAddTask} taskToUpdate={taskToUpdate} />
+      )}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
-          <TaskList tasks={tasks} />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
+          <TaskList tasks={tasks} onEdit={handleEditTask} />
         </div>
       </div>
     </section>
